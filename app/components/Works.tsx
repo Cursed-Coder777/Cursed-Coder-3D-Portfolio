555c20100a6745f97762a40314b503d1657e3ecc
Cursed-Coder777 <cursedcoder812@gmail.com>
2026-05-04 00:50:50 +0500

    Project Card scaling down

--- app/components/Works.tsx
+++ app/components/Works.tsx
@@ -37,7 +37,7 @@ const ProjectCard = ({
 			<Tilt
 				options={{
 					max: 90,
-					scale: 2,
+					scale: 1,
 					speed: 450,
 				}}
 				className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
